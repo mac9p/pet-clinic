2301abe32ad6ffc90bf2f272f1c0ddc95a047a0a
Zarkii <mleko493@wp.pl>
2021-05-13 15:16:59 +0200

    Initial commit

---
+++ src/test/java/io/github/mac9p/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mac9p.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
